8fa38483d995d06e0a0edb75165cfd9e7ed7f7a3
David Dzidepo Dumashie Ahiaku <daviddumashie@gmail.com>
2026-05-29 15:54:24 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+#File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: David Dumashie Dzidepo Ahiaku
+# STUDENT ID:  2425403635
+# Email: 2425403635@gctu.live.edu.gh
+# Grader: Augustus
+#
+# On my honor, David D.D Ahiaku, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
+    #This main function would print out my initials in stylized block letters
+    print()
+    print("...DDA")
+    print()
+    print("...DDDDDDDDD...........DDDDDDDDD................AA..........")
+    print("...DDDDDDDDDD..........DDDDDDDDDD..............AAAA.........")
+    print("...DDD......DD.........DDD......DD............AA..AA........")
+    print("...DDD.......DD........DDD.......DD..........AA....AA.......")
+    print("...DDD.......DD........DDD.......DD.........AA......AA......")
+    print("...DDD.......DD........DDD.......DD........AAAAAAAAAAAA.....")
+    print("...DDD......DD.........DDD......DD.........AAAAAAAAAAAA.....")
+    print("...DDD.....DD..........DDD.....DD..........AA........AA.....")
+    print("...DDDDDDDDD......**...DDDDDDDDD......**...AA........AA...**")
+    print("...DDDDDDDD.......**...DDDDDDDD.......**...AA........AA...**")
+    print()
 
 
 main()
+
